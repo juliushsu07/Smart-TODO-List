a454af74a0bd524b2e70a9a63eb3b9bffac7e49a
Julius Hsu <juliushsu07@gmail.com>
2017-11-22 20:15:09 +0000

    made items.js DRY

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,27 @@
 $(() => {
 
-  $('.read').on('click',()=>{
-    window.location.href='/read';
-  });
-  $('.watch').on('click',()=>{
-    window.location.href='/watch';
-  });
-  $('.buy').on('click',()=>{
-    window.location.href='/buy';
-  });
-  $('.eat').on('click',()=>{
-    window.location.href='/eat';
-  });
+  // $('.read').on('click',()=>{
+  //   window.location.href='/read';
+  // });
+  // $('.watch').on('click',()=>{
+  //   window.location.href='/watch';
+  // });
+  // $('.buy').on('click',()=>{
+  //   window.location.href='/buy';
+  // });
+  // $('.eat').on('click',()=>{
+  //   window.location.href='/eat';
+  // });
 
+  $('.lists').on('click', function(event) {
+
+    event.target.className;
+    console.log(this);
+
+    console.log( event.target.className );
+
+    window.location.href=`/${event.target.className}`;
+  });
 
   // $.ajax({
   //   method: "GET",
