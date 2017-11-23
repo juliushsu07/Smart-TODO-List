2d752c47ecf090840cbdc0f7e262ff41f218973a
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-23 19:07:21 +0000

    changed google.js so it is a module

--- api/google.js
+++ api/google.js
@@ -1,42 +1,18 @@
 const request = require('request');
-
-require('dotenv').config();
 const clientKey = process.env.DB_GOOGLE_KEY;
 
-console.log(clientKey);
+modules.exports = function googleAPI(text, callback) {
 
-// googleAPI = function(text) {
-//   request
-//   .get(`https://www.googleapis.com/customsearch/v1?q=${text}&cx=003159225023571024600%3Apw3tqlhkvcu&num=1&key=${clientKey}`)
-//   .on('response', function (response) {
-//     console.log(response.body);
-//   });
-//   };
-
-// googleAPI('Dunkirk')
-
-
-function googleAPI(text, callback) {
-  let requestOptions = {
-    url : `https://www.googleapis.com/customsearch/v1?q=${text}&cx=003159225023571024600%3Apw3tqlhkvcu&num=1&key=${clientKey}`
-  };
-
-  console.log('sending...')
-  request(requestOptions, function(err, req, body) {
+  request(`https://www.googleapis.com/customsearch/v1?q=${text}&cx=003159225023571024600%3Apw3tqlhkvcu&num=1&key=${clientKey}`
+    , function(err, req, body) {
     if(req.statusCode === 200) {
-      console.log('OK');
-      callback(JSON.parse(body));
+      const lookup = {
+        "www.yelp.com": "eat",
+      };
+      callback(null, JSON.parse(body));
     }
     else {
-      console.log('Status Code: ', req.statusCode);
-      if (req.statusCode === 404) {
-        console.log('404 ERROR');
-      } else if (req.statusCode === 401) {
-        console.log('ERROR 401');
-      }
+      callback({error: '#google_problems'});
     }
   });
-}
-
-googleAPI('Shantaram', console.log)
-
+};
