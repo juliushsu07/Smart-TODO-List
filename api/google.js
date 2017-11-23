76b797be64b5d4126997910a24c5cf8301c7fb96
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-23 19:30:45 +0000

    integrated auto sorting

--- api/google.js
+++ api/google.js
@@ -1,17 +1,24 @@
 const request = require('request');
 const clientKey = process.env.DB_GOOGLE_KEY;
 
-modules.exports = function googleAPI(text, callback) {
+require('dotenv').config();
+
+module.exports = function googleAPI(text, callback) {
 
   request(`https://www.googleapis.com/customsearch/v1?q=${text}&cx=003159225023571024600%3Apw3tqlhkvcu&num=1&key=${clientKey}`
-    , function(err, req, body) {
+          , function(err, req, body) {
     if(req.statusCode === 200) {
       const lookup = {
-        "www.yelp.com": "eat",
+        "www.yelp.ca": "eat",
+        "www.imdb.com": "watch",
+        "www.goodreads.com": "read",
+        "www.amazon.ca": "buy"
       };
-      callback(null, JSON.parse(body));
+      callback(null, lookup[JSON.parse(body).items[0].displayLink], JSON.parse(body).items[0].title);
     }
     else {
+      console.log(clientKey);
+      console.log(JSON.parse(body));
       callback({error: '#google_problems'});
     }
   });
