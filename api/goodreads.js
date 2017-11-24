7d3d99e65d977d57b9494385f6864101366f27e7
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-24 16:03:45 +0000

    querying goodreads API and getting back xml

--- api/goodreads.js
+++ api/goodreads.js
@@ -2,18 +2,21 @@
 const request   = require('request');
 const clientKey = process.env.GOODREADS_KEY;
 
-module.exports = function googleAPI(text, callback) {
+function goodreadsAPI(text, callback) {
 
   request(`https://www.goodreads.com/search/index.xml?q=harry+potter&key=oYQ4Rnj1luJbD352YA3Q`
           , function(err, req, body) {
     if(req.statusCode === 200) {
-
-      callback(null, lookup[JSON.parse(body).items[0].displayLink], JSON.parse(body).items[0].title);
+      console.log(body);
     }
     else {
       console.log(clientKey);
-      console.log(JSON.parse(body));
+      console.log(err);
       callback({error: '#google_problems'});
     }
   });
 };
+
+goodreadsAPI('harry potter', console.log);
+
+module.exports = goodreadsAPI;
