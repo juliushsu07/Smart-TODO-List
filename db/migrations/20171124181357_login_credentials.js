3f8a3814566ef228715eeab5f6a4000277d50802
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-24 19:40:07 +0000

    added all the code from the morning lecture, seems to be bug free

--- db/migrations/20171124181357_login_credentials.js
+++ db/migrations/20171124181357_login_credentials.js
@@ -3,6 +3,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.table('users', function(table){
     table.string('email');
     table.string('password');
+
+    //SHOULD MAYBE BE:
+        // table.string('email').unique().notNullable();
+        // table.string('password_digest').notNullable();
   });
 };
 
